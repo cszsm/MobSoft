18c62f743b7e255b6b3d3a7a6341f807b47bd81a
Zsolt <zscseh93@gmail.com>
2017-03-31 11:04:14 +0200

    Changes again

---
+++ app/src/main/java/com/moblab/zsolt/moblab/ui/Presenter.java
@@ -0,0 +1,13 @@
+package com.moblab.zsolt.moblab.ui;
+
+public abstract class Presenter<S> {
+    protected S screen;
+
+    public void attachScreen(S screen) {
+        this.screen = screen;
+    }
+
+    public void detachScreen() {
+        this.screen = null;
+    }
+}
